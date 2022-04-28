229765e54d4ee159f4da369790417c0358673464
Tristan Collins <tpcollins@crimson.ua.edu>
2022-04-28 13:58:41 -0500

    init commit

---
+++ MIS421FinalProject/Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace MIS421FinalProject.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
