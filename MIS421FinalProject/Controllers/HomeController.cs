cda2b0449bd08e1f36215e3b46e4426f81930b2c
zgattrell <zacharygattrell@gmail.com>
2022-04-28 19:22:49 -0500

    updated homepage

--- MIS421FinalProject/Controllers/HomeController.cs
+++ MIS421FinalProject/Controllers/HomeController.cs
@@ -1,21 +1,32 @@
-﻿using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc;
+using MIS421FinalProject.Data;
 using MIS421FinalProject.Models;
 using System.Diagnostics;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc.Rendering;
+using Microsoft.EntityFrameworkCore;
 
 namespace MIS421FinalProject.Controllers
 {
     public class HomeController : Controller
     {
         private readonly ILogger<HomeController> _logger;
+        private readonly ApplicationDbContext _context;
 
-        public HomeController(ILogger<HomeController> logger)
+        public HomeController(ILogger<HomeController> logger, ApplicationDbContext context)
         {
             _logger = logger;
+            _context = context;
         }
 
-        public IActionResult Index()
+        public async Task<IActionResult> Index()
         {
-            return View();
+            var applicationDbContext = _context.MyFood.Include(m => m.Food);
+            return View(await applicationDbContext.ToListAsync());
+            //return View();
         }
 
         public IActionResult Privacy()
