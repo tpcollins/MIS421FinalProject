315e4a0bbf88998f2dc5183a355069c8e0f10890
zgattrell <zacharygattrell@gmail.com>
2022-05-02 18:40:53 -0500

    created view model and finish homepage

---
+++ MIS421FinalProject/Views/FoodExerciseVM.cs
@@ -0,0 +1,10 @@
+using MIS421FinalProject.Models;
+
+namespace MIS421FinalProject.Views
+{
+    public class FoodExerciseVM
+    {
+        public List<MyFood> MyFoods { get; set; }
+        public List<MyExercise> MyExercise { get; set; }
+    }
+}
